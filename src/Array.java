93797dfec335a76303146f15ffb163421ae049fd
SteezyKyd <steezykydweb@gmail.com>
2022-10-15 01:33:27 +0300

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,92 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    int[][] myArray;
+    private Scanner sc;
+    int sizeWidth;
+    int sizeHeight;
+
+    Array(int sizeWidth,int sizeHeight){
+        this.sizeWidth = sizeWidth;
+        this.sizeHeight = sizeHeight;
+        this.myArray = new int[sizeWidth][sizeHeight];
+    }
+
+    void findMin(){
+        int minElement = 2147483647;
+        for(int i=0; i<myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                if (myArray[i][j] < minElement){
+                    minElement = myArray[i][j];
+                }
+            }
+        }
+        System.out.println("Minimum element: "+minElement);
+    }
+
+    void findMax(){
+        int maxElement = 0;
+        for(int i=0; i<myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                if (myArray[i][j] > maxElement){
+                    maxElement = myArray[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum element: "+maxElement);
+    }
+
+    void showMyArray(){
+        System.out.println("Here is your array:");
+        for (int i = 0; i < myArray.length; i++)
+        {
+            for (int j = 0; (myArray[i] != null && j < myArray[i].length); j++) {
+                System.out.print(myArray[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    void findAverage(){
+        int arraySum = 0;
+        for(int i=0; i<myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                arraySum+=myArray[i][j];
+            }
+        }
+        double arrayAverage = Double.valueOf(arraySum)/(Double.valueOf(sizeWidth)*Double.valueOf(sizeHeight));
+        System.out.println("Average: "+arrayAverage);;
+    }
+
+    void findAverageGeometric(){
+        int arrayMultiply = 1;
+        for(int i=0; i<myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                arrayMultiply*=myArray[i][j];
+            }
+        }
+
+        double degree = 1/(Double.valueOf(sizeWidth)*Double.valueOf(sizeHeight));
+        double arrayAverageGeometric = Math.pow(arrayMultiply, degree);
+        System.out.println("Average Geometric: "+arrayAverageGeometric);;
+    }
+
+
+
+    void setArray(){
+        for(int i=0; i<myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                System.out.println("Enter the "+i+"/"+j+" element");
+                Integer currentItem = Integer.valueOf(scanner.nextLine());
+                myArray[i][j] = currentItem;
+            }
+        }
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        start();
+
+    }
+
+    public static void start(){
+
+        try {
+            System.out.println("Enter the width of your matrix (n)");
+            Integer sizeWidth = Integer.valueOf(scanner.nextLine());
+
+            System.out.println("Enter the height of your matrix");
+            Integer sizeHeight = Integer.valueOf(scanner.nextLine());
+
+            if (sizeHeight<1 | sizeHeight>20){
+                System.out.println("Incorrect value, the allowable range of variables is from 1 to 20");
+                start();
+            } else if (sizeWidth<1 | sizeWidth>20) {
+                System.out.println("Incorrect value, the allowable range of variables is from 1 to 20");
+                start();
+            }
+
+            Array myArray = new Array(sizeWidth,sizeHeight);
+            myArray.setArray();
+            mainFunctions(myArray);
+
+        }catch (Exception ex){
+            System.out.println("Incorrect value, try again with the numbers");
+            start();
+        }
+
+    }
+    public static void mainFunctions(Array myArray){
+
+        System.out.println("Choose your option, enter the number: \n1)Show my array \n2)Find min \n3)Find max \n4)Find average \n5)Find average geometric \n6)Choose another option \n7)Restart \n8)Exit");
+
+        try {
+            Integer currentOption = Integer.valueOf(scanner.nextLine());
+            switch (currentOption){
+                case 1:
+                    myArray.showMyArray();
+                    mainFunctions(myArray);
+                    break;
+                case 2:
+                    myArray.findMin();
+                    mainFunctions(myArray);
+                    break;
+                case 3:
+                    myArray.findMax();
+                    mainFunctions(myArray);
+                    break;
+                case 4:
+                    myArray.findAverage();
+                    mainFunctions(myArray);
+                    break;
+                case 5:
+                    myArray.findAverageGeometric();
+                    mainFunctions(myArray);
+                    break;
+                case 6:
+                    mainFunctions(myArray);
+                    break;
+                case 7:
+                    start();
+                    break;
+                case 8:
+                    System.out.println("Zzz...");
+                    System. exit(0);
+                    break;
+                default:
+                    System.out.println("Error! Choose correct function!");
+                    mainFunctions(myArray);
+            }
+        }catch (Exception ex){
+            System.out.println("Incorrect value, try again with the numbers");
+            start();
+        }
+
+    }
+}
